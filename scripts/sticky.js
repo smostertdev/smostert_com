e2e08fe62c4baa2f6f8306c1f39a182709fba4c3
Sylvan Mostert <smostert.dev@gmail.com>
2016-01-17 13:10:46 -0500

    Initial Commit

---
+++ scripts/sticky.js
@@ -0,0 +1,11 @@
+	
+var nav = $('.nav-container');
+
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 136) {
+        nav.addClass("f-nav");
+    } else {
+        nav.removeClass("f-nav");
+    }
+});
+
