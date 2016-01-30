87576cd075eafd22223588618d41e32823eb4b84
Sylvan Mostert <smostert.dev@gmail.com>
2016-01-30 03:41:26 -0500

    Added sticky menu bar
    The page is changed to a width of 1100px.

--- scripts/sticky.js
+++ scripts/sticky.js
@@ -1,11 +1,51 @@
+/* Refactored by David Walsh
+ * (https://davidwalsh.name/javascript-debounce-function) */
+function debounce(callback, wait) {
+	var time;
+	return function() {
+		clearTimeout(time);
+		time = setTimeout(function() {
+			time = null;
+			callback.call();
+		}, wait);
+	}
+}
+
+window.onload = function(){
+	/* flag to decrease redraw events */
+	var sticky = false;
+	var nav    = document.getElementById("menu");
+	var header = document.getElementsByTagName("header")[0];
+
+	var nav_offset    = nav.offsetTop;
+	var header_offset = header.offsetTop;
+
+	var offs = window.pageYOffset;
 	
-var nav = $('.nav-container');
+	if(!sticky && offs > nav_offset) {
+		sticky = true;
+		nav.style.position = "fixed";
+		nav.style.top      = 0 + "px";
+		nav.style.width    = header.offsetWidth + "px";
+	}
 
-$(window).scroll(function () {
-    if ($(this).scrollTop() > 136) {
-        nav.addClass("f-nav");
-    } else {
-        nav.removeClass("f-nav");
-    }
-});
+	scrollHandler = debounce(function() {
+		offs = window.pageYOffset;
 
+		if(!sticky && offs > nav_offset) {
+			sticky = true;
+			nav.style.position = "fixed";
+			nav.style.top      = 0 + "px";
+			nav.style.width    = header.offsetWidth + "px";
+			
+		}
+
+		if(sticky && offs < nav_offset) {
+			sticky = false;
+			nav.style.position = "relative";
+		}
+	}, 5);
+
+	window.onscroll = scrollHandler;
+
+}
