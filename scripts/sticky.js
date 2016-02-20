7b3b7cb9d4fc6ed1950769b97094d0460cdebc47
Sylvan Mostert <smostert.dev@gmail.com>
2016-02-20 01:19:22 -0500

    Fixed page jumping on menu position change.
    Added a <div> at bottom of menu to fill space when it becomes fixed.

--- scripts/sticky.js
+++ scripts/sticky.js
@@ -15,6 +15,7 @@ window.onload = function(){
 	/* flag to decrease redraw events */
 	var sticky = false;
 	var nav    = document.getElementById("menu");
+	var fill   = document.getElementById("fill");
 	var header = document.getElementsByTagName("header")[0];
 
 	var nav_offset    = nav.offsetTop;
@@ -24,6 +25,7 @@ window.onload = function(){
 	
 	if(!sticky && offs > nav_offset) {
 		sticky = true;
+		fill.style.height  = 40 + "px";
 		nav.style.position = "fixed";
 		nav.style.top      = 0 + "px";
 		nav.style.width    = header.offsetWidth + "px";
@@ -34,6 +36,7 @@ window.onload = function(){
 
 		if(!sticky && offs > nav_offset) {
 			sticky = true;
+			fill.style.height  = 40 + "px";
 			nav.style.position  = "fixed";
 			nav.style.top       = 0 + "px";
 			nav.style.width     = header.offsetWidth + "px";
@@ -44,6 +47,7 @@ window.onload = function(){
 			sticky = false;
 			nav.style.position = "relative";
 			nav.style.boxShadow = "none";
+			fill.style.height  = 0 + "px";
 		}
 	}, 5);
 
